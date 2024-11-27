325004f8ff7136a5655db86c50e69cbf64751c5c
Haobo Yan <wittscien@gmail.com>
2024-11-27 17:18:52 +0100

    Fix proj su3.

--- lqcd/utils/utils.py
+++ lqcd/utils/utils.py
@@ -7,5 +7,5 @@ def proj_su3(A):
     # A: 3 x 3 complex matrix
     xp = get_backend()
     Ap = A @ xp.linalg.inv(xp.array(sp.linalg.sqrtm(xp.conjugate(xp.transpose(A)) @ A),dtype=complex))
-    Ap = Ap / xp.linalg.det(A) ** (1/3)
+    Ap = Ap / xp.linalg.det(Ap) ** (1/3)
     return Ap
